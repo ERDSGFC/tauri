2550499abfae1689fbec35995bc04382b65eae9f
秦瑞涵 <3097727187@qq.com>
2023-09-14 23:49:52 +0800

    窗口功能

--- src/components/top-toolbar.vue
+++ src/components/top-toolbar.vue
@@ -1,15 +1,11 @@
 <script setup>
 import { appWindow } from "@tauri-apps/api/window";
-import { ref, onMounted } from "vue";
-const max_flag = ref(0)
+import { ref } from "vue";
+const max_flag = ref(false)
 
-function test() {
-   console.log(1);
+function toggleMax() {
+   max_flag.value = ! max_flag.value
    appWindow.toggleMaximize()
-}
-
-function winMax() {
-   appWindow.maximize()
 }
 function winMin() {
    appWindow.minimize()
@@ -17,34 +13,32 @@ function winMin() {
 function winClose() {
    appWindow.close()
 }
-onMounted(() => {
-   console.log(1)
-})
-
 </script>
 <template>
-   <div class="h-9 flex flex-row justify-between items-center content-center">
+   <div data-tauri-drag-region class="h-9 flex flex-row justify-between items-center">
       <!-- left -->
-      <div class="bg-blue-600 tool-bar-left flex">
-         <div @click="test">设置</div>
-         <div>帮助</div>
+      <div class="tool-bar-left flex ml-2">
+         <div class="px-1.5">设置</div>
+         <div class="px-1.5">帮助</div>
       </div>
       <!-- left -->
       <div class="bg-green-500 basis-1/3 text-center">
-         <ElInput></ElInput>
-         中间文本
       </div>
       <!-- right -->
-      <div class="flex h-full justify-center items-center">
-         <div class="w-9 hover:bg-gray-800" @click="winMin">最小</div>
-         <div class="w-9 hover:bg-gray-700" @click="winMax"><i-carbon-accessibility /></div>
-         <div class="w-9 hover:bg-red-700" @click="winClose"><el-icon color="#fc8c23"><i-mingcute-close-fill /></el-icon> </div>
+      <div class="flex h-full justify-center">
+         <div class="w-9 hover:bg-gray-400 text-center" @click="winMin"><i-ant-design-minus-outlined class="h-9" /></div>
+         <div class="w-9 hover:bg-gray-400 text-center" @click="toggleMax">
+            <i-ant-design-fullscreen-exit-outlined v-show="max_flag" class="h-9" />
+            <i-ant-design-fullscreen-outlined v-show="max_flag==false" class="h-9"/>
+         </div>
+         <div class="w-9 hover:bg-red-700 text-center " @click="winClose"><i-ant-design-close-outlined class="h-9" /> </div>
       </div>
    </div>
 </template>
 <style scoped>
 .tool-bar-left > div:hover  {
-   background-color: #3e3841;
+   --tw-bg-opacity: 1;
+   background-color: rgba(156, 163, 175, var(--tw-bg-opacity));
    border-radius: 0.3rem;
 }
 </style>
